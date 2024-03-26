210f41f92e863da63f273a61ba6cce6d1b35ef6e
Avish Khandelwal <81549108+Avishk12@users.noreply.github.com>
2024-03-26 21:11:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import helper
 
 image = Image.open('mitaoe-logo.jpg')
 st.image(image)
-st.header('Enhancing Underwater Images with Color Balance and Fusion')
-st.text('Final Year CV Group Project')
+st.header('Enhancing Underwater Images')
+st.text('Final Year Capstone Project')
 st.text('Credits: ')
-st.text('B234087 Piyush Bhondave')
 st.text('B238034 Avish Khandelwal')
-st.text('B234037 Rutuja Mahajan')
-st.text('B234116 Akanksha Madamanchi')
 st.divider()
-st.header('Upload the Underwate Image (.png or .jpg or .jpeg)')
+st.header('Upload the Underwater Image (.png or .jpg or .jpeg)')
 img_main = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
 st.divider()
 if img_main is not None:
